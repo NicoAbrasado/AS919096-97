f7d5f1a548017fc92d17546a5cb11b598c62a252
nicoa <18207@students.mrgs.school.nz>
2022-06-08 10:10:15 +1200

    Added functionality to register button Done this by adding new activity and an onclick listener with new intents to open the new activity.

--- app/src/main/java/com/example/as919097/MainActivity.java
+++ app/src/main/java/com/example/as919097/MainActivity.java
@@ -2,15 +2,34 @@ package com.example.as919097;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button RegisterButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        // Declare the toolbar in the xml file
         setSupportActionBar(findViewById(R.id.toolbar));
+
+        // Register Button, opening the register activity
+        RegisterButton = (Button) findViewById(R.id.register_button);
+        RegisterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openregister();
+            }
+        });
+    }
+
+    public void openregister() {
+        Intent intent = new Intent(this, register.class);
+        startActivity(intent);
     }
 }
